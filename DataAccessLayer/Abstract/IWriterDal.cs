f7125bd0648fe02081e4c7086b68583e3f877fb7
gozdemogus <gozdemog@gmail.com>
2022-10-16 21:12:53 +0000

    generic repository

---
+++ DataAccessLayer/Abstract/IWriterDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IWriterDal:IRepository<Writer>
+    {
+    }
+}
