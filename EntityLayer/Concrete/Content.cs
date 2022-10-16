efefef75d5a602896833626c0ca9dacfc7800ec1
gozdemogus <gozdemog@gmail.com>
2022-10-16 18:31:59 +0000

    migrations

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,16 @@ namespace EntityLayer.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentId { get; set; }
+
+        [StringLength(1000)]
         public string ContentValue  { get; set; }
+
         public DateTime ContentDate { get; set; }
 
-
         //content writer ile iliski
-        public int WriterID { get; set; }  
+        public int? WriterID { get; set; }
         public virtual Writer Writer { get; set; }
 
         //content header ile iliski
