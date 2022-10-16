bd27d44f48fb555ea27b42fdeb9fa2106da3e5b2
gozdemogus <gozdemog@gmail.com>
2022-10-16 17:21:03 +0000

    entity layer

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public string ContentValue  { get; set; }
+        public DateTime ContentDate { get; set; }
+
+
+        //content writer ile iliski
+        public int WriterID { get; set; }  
+        public virtual Writer Writer { get; set; }
+
+        //content header ile iliski
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int HeadingId { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+
+        //bunlar ikili paket gibi oluyor 
+
+        //iliskili tablonun anahtar sütunuyla aynı isimde verilmeli!!!!
+        //Heading tablosu icinde CategoryID isimli bi sütun olacak
+        //bire cok iliski
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+   
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        //ikinci iliski de contentle, bire cok iliski, burası bir tarafında
+        public ICollection<Content> Contents { get; set; }
+
+
+    }
+}
