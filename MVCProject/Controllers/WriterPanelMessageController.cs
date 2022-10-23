08b96a95b3b3f88b604a39019b9eb66be9e17e9f
gozdemogus <gozdemog@gmail.com>
2022-10-23 16:43:50 +0000

    message & content management

---
+++ MVCProject/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,83 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+
+        MessageManager mm = new MessageManager(new EFMessageDal());
+        MessageValidator messageValidator = new MessageValidator();
+        // GET: WriterPanelMessage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Inbox()
+        {
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+
+
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSentBox();
+            return View(messagelist);
+        }
+
+        public PartialViewResult MessageListMenu()
+        {
+
+            return PartialView();
+        }
+        public ActionResult GetInboxMessageDetails(int id)
+        {
+            var values = mm.GetById(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendBoxMessageDetails(int id)
+        {
+            var values = mm.GetById(id);
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult results = messageValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.SenderMail = "gizem@gmail.com";
+                
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("SendBox");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Controllers/WriterPnlController.cs
@@ -0,0 +1,84 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class WriterPnlController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());   
+        // GET: WriterPnl
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public ActionResult MyHeading()
+        {
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+
+       
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.vlc=valuecategory;  
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 4;
+            p.HeadingStatus = true;
+            hm.Headingadd(p);
+            return RedirectToAction("MyHeading");
+        
+        }
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            var HeadingValue = hm.GetById(id);
+            return View(HeadingValue);
+        }
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            hm.Headingupdate(p);
+            return RedirectToAction("MyHeading");
+
+        }
+
+
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetById(id);
+            HeadingValue.HeadingStatus = false;
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("MyHeading");
+        }
+
+
+    }
+}
