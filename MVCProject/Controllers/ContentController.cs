6b41bcfb9c5503c32db3e284ebdb781ba23990c6
gozdemogus <gozdemog@gmail.com>
2022-10-25 11:40:19 +0000

    iframe,writer page,home page,improvements

--- MVCProject/Controllers/ContentController.cs
+++ MVCProject/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,16 @@ namespace MVCProject.Controllers
         {
             return View();
         }
+
+        Context c = new Context();
+        public ActionResult GetAllContent(string p="")
+        {
+            //var values = from x in c.Contents select x;
+            var values=cm.GetList(p);
+          
+            return View(values.ToList());
+        }
+
 
         public ActionResult ContentByHeading(int id)
         {
